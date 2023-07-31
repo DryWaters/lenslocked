0ed25a1f6d1d3a60269f0a4ad503f5b0428d1f06
Daniel Waters <danwater1@gmail.com>
2023-07-30 22:51:30 -0400

    Adds CHI router

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"net/http"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Welcome to my awesome site!</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintf(w, "<h1>Contact Page</h1><p>To get in touch, email me at "+
+		"<a href=\"mailto:daniel@bitofbytes.io\">daniel@bitofbytes.io</a>")
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintf(w, "<h1>FAQ Page</h1><ul><li>Q: Is there a free version?</li><li>A: Yes! We offer a free trial for 30 days on any paid plans</li></ul>")
+}
+
+func main() {
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
+	fmt.Println("Starting the server on :3000...")
+	http.ListenAndServe(":3000", r)
+}
