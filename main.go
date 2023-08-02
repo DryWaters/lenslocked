9a9a02b3181c7f3f4d1c22d099f8e0a9844c6bb4
Daniel Waters <danwater1@gmail.com>
2023-08-01 23:01:17 -0400

    Refactored home handler to use templates

--- main.go
+++ main.go
@@ -3,11 +3,28 @@ package main
 import (
 	"fmt"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Welcome to my awesome site!</h1>")
+	w.Header().Set("Content-Type", "text/html")
+	tplPath := filepath.Join("templates", "home.gohtml")
+	tpl, err := template.ParseFiles(tplPath)
+	if err != nil {
+		log.Printf("parsing template: %v", err)
+		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
+		return
+	}
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("executing template: %v", err)
+		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
+		return
+	}
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
@@ -18,11 +35,17 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprintf(w, "<h1>FAQ Page</h1><ul><li>Q: Is there a free version?</li><li>A: Yes! We offer a free trial for 30 days on any paid plans</li></ul>")
+	fmt.Fprintf(w, `<h1>FAQ Page</h1>
+<ul>
+	<li><b>Is there a free version?</b> Yes! We offer a free trial for 30 days on any paid plans.</li>
+	<li><b>What are your support hours?</b> We have support staff answering emails 24/7, though response times may be a bit slower on weekends.</li>
+	<li><b>How do I contact support?</b> Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a></li>
+</ul>`)
 }
 
 func main() {
 	r := chi.NewRouter()
+	r.Use(middleware.Logger)
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
 	r.Get("/faq", faqHandler)
