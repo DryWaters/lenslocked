b8c9b306fd1b6bfe015a6961bb522ccbfad75a89
Daniel Waters <danwater1@gmail.com>
2023-08-08 23:41:02 -0400

    Created Template interface

--- controllers/users.go
+++ controllers/users.go
@@ -1,17 +1,15 @@
 package controllers
 
 import (
-	"github.com/drywaters/lenslocked/views"
 	"net/http"
 )
 
 type Users struct {
 	Templates struct {
-		New views.Template
+		New Template
 	}
 }
 
 func (u Users) New(w http.ResponseWriter, r *http.Request) {
-	// We need a view to render
 	u.Templates.New.Execute(w, nil)
 }
